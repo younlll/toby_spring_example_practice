2fc4211f20707e7de4124c4d7492f4af8611b0ee
younlll <youn.lee96@gmail.com>
2022-02-27 10:01:59 +0900

    init: Create folder and connect to DB

---
+++ practice/src/test/java/tobyspring/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
