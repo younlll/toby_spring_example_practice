9e002f43f32e9e141899bd8af93798788c29ec2c
younlll <youn.lee96@gmail.com>
2022-02-27 16:14:05 +0900

    modify: N or D UserDao db connection

--- practice/src/main/java/tobyspring/practice/user/dao/DUserDao.java
+++ practice/src/main/java/tobyspring/practice/user/dao/DUserDao.java
@@ -1,11 +1,15 @@
 package tobyspring.practice.user.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DUserDao extends UserDao {
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         // D사 DB connection 생성코드
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/spring_study", "root", "root");
+        return c;
     }
 }
